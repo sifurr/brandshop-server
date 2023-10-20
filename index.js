a9e1d130ec7bff5323763f0bd029f3145a7386d7
Md Saifur Rahman <saifurrshishir@gmail.com>
2023-10-20 12:39:47 +0600

    Add cart api to store the requested product id and user email

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -26,42 +26,85 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // database and collection
-    const productCollection = client.db("brandShopDB").collection("productCollection");
-    const brandCollection = client.db("brandShopDB").collection("brandCollection");
-    
+    const productCollection = client
+      .db("brandShopDB")
+      .collection("productCollection");
+    const brandCollection = client
+      .db("brandShopDB")
+      .collection("brandCollection");
+    const cartCollection = client
+      .db("brandShopDB")
+      .collection("cartCollection");
+
     /// API for Brands
     // get api - for all the brands
-    app.get('/brands', async (req, res)=>{
-        const cursor = brandCollection.find();
-        const result = await cursor.toArray();
-        res.send(result);
-    })
+    app.get("/brands", async (req, res) => {
+      const cursor = brandCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
     // post api - for a brand
-    app.post('/brands', async (req, res)=>{
-        const receivedBrandInfo = req.body;
-        const result = await brandCollection.insertOne(receivedBrandInfo);
-        res.send(result);
-    })
+    app.post("/brands", async (req, res) => {
+      const receivedBrandInfo = req.body;
+      const result = await brandCollection.insertOne(receivedBrandInfo);
+      res.send(result);
+    });
 
     // API for Products
     // get api - for all the products
-    app.get('/products', async (req, res)=>{
-        const cursor = productCollection.find();
-        const result = await cursor.toArray();
-        res.send(result);
-    })
+    app.get("/products", async (req, res) => {
+      const cursor = productCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    // get api - for a product
+    app.get("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await productCollection.findOne(query);
+      res.send(result);
+    });
 
     // post api - for a single product
-    app.post('/products', async (req, res)=>{
-        const receivedProductInfo = req.body;
-        const result = await productCollection.insertOne(receivedProductInfo);
-        res.send(result);
-    })
+    app.post("/products", async (req, res) => {
+      const receivedProductInfo = req.body;
+      const result = await productCollection.insertOne(receivedProductInfo);
+      res.send(result);
+    });
 
-  
+    // post api - for a product id for the cart
+    app.post("/carts", async (req, res) => {
+      const receivedProductId = req.body;
+      const idAsResult = await cartCollection.insertOne(receivedProductId);
+      res.send(idAsResult);
+    });
 
-
+    // put api - for a single product
+    app.put("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updateRequest = req.body;
+      const updatedInfo = {
+        $set: {
+          name: updateRequest.name,
+          brandName: updateRequest.brandName,
+          image: updateRequest.image,
+          price: updateRequest.price,
+          rating: updateRequest.rating,
+          type: updateRequest.type,
+          shortDescription: updateRequest.shortDescription,
+        },
+      };
+      const result = await productCollection.updateOne(
+        filter,
+        updatedInfo,
+        options
+      );
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
